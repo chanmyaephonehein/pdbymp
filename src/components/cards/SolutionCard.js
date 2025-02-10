3682fb3f8909a5f278dd80ed256207f01b3b402d
Jayson <jaysonchenn17@gmail.com>
2025-02-10 21:48:12 +0700

    card of solutions add

--- src/components/cards/SolutionCard.js
+++ src/components/cards/SolutionCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SolutionCard = ({ title, content, list, button }) => {
   return (
-    <div className="rounded-lg shadow-lg w-2/5 h-96  flex flex-col items-center justify-between px-4 py-10">
+    <div className="rounded-lg shadow-lg w-2/5 h-[420px]  flex flex-col items-center justify-between px-4 py-10">
       <div className="flex flex-col">
         <p className="text-3xl align-center pb-5 text-center px-10">{title}</p>
         <p className="pb-4 text-lg px-4 text-justify">{content}</p>
