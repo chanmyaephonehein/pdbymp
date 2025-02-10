4f4043759a6c8b62e2a255407980212affc21c62
Jayson <jaysonchenn17@gmail.com>
2025-02-10 21:51:22 +0700

    UI fix

--- src/components/cards/SolutionCard.js
+++ src/components/cards/SolutionCard.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const SolutionCard = ({ title, content, list, button }) => {
   return (
-    <div className="rounded-lg shadow-lg w-2/5 h-[420px]  flex flex-col items-center justify-between px-4 py-10">
+    <div className="rounded-lg shadow-lg w-2/5 h-96  flex flex-col items-center justify-between px-4 py-10">
       <div className="flex flex-col">
-        <p className="text-3xl align-center pb-5 text-center px-10">{title}</p>
+        <p className="text-2xl align-center pb-4 text-center font-bold px-10">
+          {title}
+        </p>
         <p className="pb-4 text-lg px-4 text-justify">{content}</p>
         <ul className="list-disc pl-24">
           {list.map((item, index) => (
