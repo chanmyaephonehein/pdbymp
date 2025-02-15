79316ace6e6b553ff67ea3af96b9bf1fdd5f9c8e
Jayson <jaysonchenn17@gmail.com>
2025-02-16 00:42:30 +0700

    Home Page

---
+++ src/components/cards/IndustryImgCard.js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const IndustryImgCard = ({ source, title, route }) => {
+  const router = useRouter();
+  return (
+    <div
+      onClick={() => router.push({ pathname: `${route}` })}
+      className="bg-gray-900 rounded-lg p-4 cursor-pointer"
+    >
+      <img src={source} className="w-full rounded-lg" alt={title} />
+      <div className="flex justify-center mt-2">
+        <p className="text-xl font-semibold text-white">{title}</p>
+      </div>
+    </div>
+  );
+};
+
+export default IndustryImgCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,72 @@
+import IndustryImgCard from "@/components/cards/IndustryImgCard";
 import SolutionCard from "@/components/cards/SolutionCard";
 
+const images = [
+  {
+    id: 1,
+    title: "Education",
+    url: "/image2.png",
+    route: "/user/industries/1",
+  },
+  {
+    id: 2,
+    title: "Finance  ",
+    url: "/image3.png",
+    route: "/user/industries/1",
+  },
+  {
+    id: 3,
+    title: "Healthcare",
+    url: "/image4.png",
+    route: "/user/industries/2",
+  },
+  {
+    id: 4,
+    title: "Government",
+    url: "/image5.png",
+    route: "/user/industries/3",
+  },
+];
 const Home = () => {
   return (
-    <>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-      <p>Das ist Home Page</p>
-    </>
+    <div className="flex flex-col items-center">
+      <img src="/image20.png" className="w-1/2 rounded-lg mb-20" />
+      <div className="flex gap-2 items-center">
+        <div className="flex flex-col gap-3 text-center">
+          <p className="text-3xl font-semibold">
+            Empowering Tomorrow with Intelligent Solutions Today
+          </p>
+          <div className="flex justify-center">
+            <p className="text-xl w-2/3 text-center">
+              Transform Your Business with Custom AI Solutions Built for
+              Scalability, Efficiency, and Innovation.
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <p className="text-lg w-5/6">
+              At AI-Solution, our mission is to transform complex challenges
+              into opportunities through cutting-edge AI technology. We empower
+              businesses to innovate, optimize, and lead in their industries
+              with ethical, scalable solutions.
+            </p>
+          </div>
+        </div>
+        <img src="/image1.png" className="w-1/4 rounded-lg" />
+      </div>
+      <p className="mt-20 mb-10 text-3xl font-semibold">
+        Solutions By Industies
+      </p>
+      <div className="flex gap-10">
+        {images.map((item) => (
+          <IndustryImgCard
+            key={item.id}
+            source={item.url}
+            title={item.title}
+            route={item.route}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
