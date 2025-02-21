0286846f21a2da2ce88f4de7445ef66fe80272c1
Jayson <jaysonchenn17@gmail.com>
2025-02-21 18:45:40 +0700

    country select

--- src/pages/user/contactus/index.js
+++ src/pages/user/contactus/index.js
@@ -1,4 +1,10 @@
+import Select from "react-select";
+import countryList from "react-select-country-list";
+import { useState } from "react";
+
 const ContactUs = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const countries = countryList().getData();
   return (
     <div className="flex justify-center px-4 sm:px-8 md:px-16 lg:px-24 ">
       <div className="w-full bg-gray-900 p-8 flex flex-col md:flex-row items-center md:items-start">
@@ -28,13 +34,16 @@ const ContactUs = () => {
             <label className="block text-sm font-medium">Company Name</label>
             <input type="text" className="w-full p-2 mb-3 border rounded-md" />
 
-            <label className="block text-sm font-medium">Country</label>
-            <select className="w-full p-2 mb-3 border rounded-md">
-              <option>Select a country</option>
-              <option>USA</option>
-              <option>UK</option>
-              <option>Canada</option>
-            </select>
+            <div>
+              <label className="block text-sm font-medium">Country</label>
+              <Select
+                options={countries}
+                value={selectedCountry}
+                onChange={setSelectedCountry}
+                placeholder="Select a country"
+                className="w-full text-black p-2 mb-3 border rounded-md"
+              />
+            </div>
 
             <label className="block text-sm font-medium">Job Title</label>
             <input type="text" className="w-full p-2 mb-3 border rounded-md" />
